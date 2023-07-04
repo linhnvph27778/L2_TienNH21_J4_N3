dc5524cc1fc73fdbb10d9983d023282e17ed7684
NhanPham2003 <117698937+nhanphph23607@users.noreply.github.com>
2023-07-04 14:08:23 +0700

    s

--- src/main/java/com/example/datn/controller/LoginController.java
+++ src/main/java/com/example/datn/controller/LoginController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/authencation")
+@RequestMapping("/authencation")
 public class LoginController {
 
-    @GetMapping("/s")
-    public String getLoginForm(Model model){
-//        model.addAttribute("user", new KhachHang());
-        return "/s";
+    @GetMapping("/login")
+    private String getLoginForm(){
+        return "/authencation/login";
     }
 
 }
