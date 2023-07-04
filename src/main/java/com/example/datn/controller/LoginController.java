37e709bc31bf30cda1626e13941ed64e1a381d6a
NhanPham2003 <117698937+nhanphph23607@users.noreply.github.com>
2023-07-04 09:23:16 +0700

    ,

--- src/main/java/com/example/datn/controller/LoginController.java
+++ src/main/java/com/example/datn/controller/LoginController.java
@@ -1,8 +1,18 @@
 package com.example.datn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@RequestMapping("/authencation")
 public class LoginController {
+
+    @GetMapping("/s")
+    public String getLoginForm(Model model){
+//        model.addAttribute("user", new KhachHang());
+        return "/s";
+    }
 
 }
