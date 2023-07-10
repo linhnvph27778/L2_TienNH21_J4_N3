ba2a3973e874bd33edf6f954aa1b88177b9a86b4
NhanPham2003 <117698937+nhanphph23607@users.noreply.github.com>
2023-07-10 16:51:50 +0700

    Code Foggot Password

--- src/main/java/com/example/datn/usersController/UserHomeController.java
+++ src/main/java/com/example/datn/usersController/UserHomeController.java
@@ -1,6 +1,9 @@
 package com.example.datn.usersController;
 
+import com.example.datn.entity.KhachHang;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserHomeController {
 
     @RequestMapping(value = {"", "/", "/usersHome", "/homeUser"})
-    private String getUsersHomeForm(){
+    private String getUsersHomeForm(Model model, HttpSession session){
 
+        KhachHang khachHang =(KhachHang)  session.getAttribute("UserLogged");
 
+        if (khachHang != null){
+            model.addAttribute("messageLoginOrSignin", false);
+        }else{
+            model.addAttribute("messageLoginOrSignin", true);
+        }
         return "/viewsUsers/usersHome";
     }
 
